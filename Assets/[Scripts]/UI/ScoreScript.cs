ef30483e469775e0631b10a3bd9ee5d5c5958325
Jesus Lawrence Osorio <97567506+JLDO2105@users.noreply.github.com>
2023-01-30 04:04:49 -0500

    Updated Score Script

--- Assets/[Scripts]/UI/ScoreScript.cs
+++ Assets/[Scripts]/UI/ScoreScript.cs
@@ -2,7 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-
 public class ScoreScript : MonoBehaviour
 {
     public static int scoreValue = 0;
