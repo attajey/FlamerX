da7d5320e3f3a57781a2a54e1b7660ac2c921b66
Atta Jirofty <jirofty76@gmail.com>
2023-02-14 10:06:47 -0500

    Added UI for Coin Collection

---
+++ Assets/CoinText.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CoinText : MonoBehaviour
+{
+    // Start is called before the first frame update
+    int coinCount = 0;
+    private Text cointText;
+
+    void Start()
+    {
+        cointText = GetComponent<Text>();
+    }
+    private void OnEnable()
+    {
+        Coin.OnCoinCollected += IncreamentCoinCount;
+    }
+
+    private void OnDisable()
+    {
+        
+        Coin.OnCoinCollected -= IncreamentCoinCount;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void IncreamentCoinCount()
+    {
+        coinCount++;
+        cointText.text = $"Coins: {coinCount}";
+        //cointText.SetText($"Coins: {coinCount}");
+    }
+}
