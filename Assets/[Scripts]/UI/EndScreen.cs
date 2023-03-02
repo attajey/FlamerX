1e5769da3cd88cafe14163d3a0624e28d5e73b81
Miles136 <nemanuel.montoya@gmail.com>
2023-03-02 16:16:51 -0500

    Working End Screen
    end screen now properly shows collected players stats, win/loss, final numbers can be tweaked later on

--- Assets/[Scripts]/UI/EndScreen.cs
+++ Assets/[Scripts]/UI/EndScreen.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class EndScreen : MonoBehaviour
 {
@@ -8,7 +9,9 @@ public class EndScreen : MonoBehaviour
     public GameObject win;
     public GameObject loss;
 
-    public GameObject textToEdit;
+    public TextMeshProUGUI textToEdit;
+
+    public int[] grades;
 
     // Start is called before the first frame update
     void Start()
@@ -21,6 +24,26 @@ public class EndScreen : MonoBehaviour
             win.SetActive( true );
             loss.SetActive( false );
         }
+
+        string finalGrade = "SABCDF";
+
+        int totalScore = GlobalVariables.totalScore;
+
+        if ( totalScore < grades[0]) finalGrade = "F";
+        else if ( totalScore < grades[1] ) finalGrade = "D";
+        else if ( totalScore < grades[2] ) finalGrade = "C";
+        else if ( totalScore < grades[3] ) finalGrade = "B";
+        else if ( totalScore < grades[4] ) finalGrade = "A";
+        else finalGrade = "S";
+
+
+        textToEdit.SetText(
+            GlobalVariables.enemiesKilled.ToString() + '\n' +
+            GlobalVariables.itemsCollected.ToString() + '\n' +
+            '\n' +
+            GlobalVariables.totalScore.ToString() + '\n' +
+            finalGrade
+            );
     }
 
     // Update is called once per frame
