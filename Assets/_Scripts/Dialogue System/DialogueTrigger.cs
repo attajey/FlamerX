0ea7b660c4c5178fe006154a0b72aba72b292c4a
Atta Jirofty <jirofty76@gmail.com>
2023-03-25 15:40:10 -0400

    Added Dialogue System - Folder Clean up
    Added dialogue system to level 1.

---
+++ Assets/_Scripts/Dialogue System/DialogueTrigger.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueTrigger : MonoBehaviour {
+
+	public Dialogue dialogue;
+
+	public void TriggerDialogue ()
+	{
+		FindObjectOfType<DialogueManager>().StartDialogue(dialogue);
+	}
+
+    public void EndDialogue()
+    {
+
+		FindObjectOfType<DialogueManager>().EndDialogue();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+		{
+			TriggerDialogue();
+		}
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            EndDialogue();
+        }
+    }
+
+}
