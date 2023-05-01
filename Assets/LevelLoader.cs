927c2c603b0ba32c0372ead9403cf7932fb536ff
Atta Jirofty <jirofty76@gmail.com>
2023-05-01 15:34:35 -0400

    Updated Level1 UI

--- Assets/LevelLoader.cs
+++ Assets/LevelLoader.cs
@@ -1,18 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class LevelLoader : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] Animator transition;
+    [SerializeField] float transitionTime = 1f;
 
-    // Update is called once per frame
-    void Update()
+    public void PlayGame()
     {
-        
+        StartCoroutine(LoadLevel(SceneManager.GetActiveScene().buildIndex + 1));
+
+    }
+    IEnumerator LoadLevel(int levelindex)
+    {
+        // Play anim
+        transition.SetTrigger("Start");
+
+        // Wait
+        yield return new WaitForSeconds(transitionTime);
+
+        // Load Scene
+        SceneManager.LoadScene(levelindex);
     }
 }
