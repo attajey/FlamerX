16d01e7a33fe0039ab2bf474de37cb49922cb0e2
Atta Jirofty <jirofty76@gmail.com>
2023-05-01 12:26:54 -0400

    Finished Main Menu

--- Assets/MainMenu.cs
+++ Assets/MainMenu.cs
@@ -2,13 +2,29 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using static System.TimeZoneInfo;
+
 public class MainMenu : MonoBehaviour
 {
+    [SerializeField] Animator transition;
+    [SerializeField] float transitionTime = 1f;
+
     public void PlayGame()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
-    }
+        StartCoroutine(LoadLevel(SceneManager.GetActiveScene().buildIndex + 1));
 
+    }
+    IEnumerator LoadLevel(int levelindex)
+    {
+        // Play anim
+        transition.SetTrigger("Start");
+
+        // Wait
+        yield return new WaitForSeconds(transitionTime);
+
+        // Load Scene
+        SceneManager.LoadScene(levelindex);
+    }
     public void ExitGame()
     {
         Debug.Log("You Closed The Game!");
--- Assets/OptionsMenu.cs
+++ Assets/OptionsMenu.cs
@@ -1,12 +1,50 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro.EditorUtilities;
 using UnityEngine;
 using UnityEngine.Audio;
+using UnityEngine.UI;
+using TMPro;
+
 
 public class OptionsMenu : MonoBehaviour
 {
-    [SerializeField] private AudioMixer audioMixer; 
-    public void SetMasterVolume (float volume)
+    [SerializeField] private AudioMixer audioMixer;
+    [SerializeField] private TMP_Dropdown resolutionDropdown;
+
+    Resolution[] resolutions;
+
+    void Start()
+    {
+        // Get player's available resolutions 
+        resolutions = Screen.resolutions;
+
+        resolutionDropdown.ClearOptions();
+
+        // Store player's available resolutions in a list
+        List<string> playerResolutionOptions = new List<string>();
+        
+        int currentResolutionIndex = 0;
+        
+        for (int i = 0; i < resolutions.Length; i++)
+        {
+            string option = resolutions[i].width + " x " + resolutions[i].height;
+            playerResolutionOptions.Add(option);
+            if (resolutions[i].width == Screen.currentResolution.width && 
+                resolutions[i].height == Screen.currentResolution.height)
+            {
+                currentResolutionIndex = i;
+            }
+        }
+
+        // Add player's available resolutions to the menu dropdown
+        resolutionDropdown.AddOptions(playerResolutionOptions);
+        resolutionDropdown.value= currentResolutionIndex;
+        resolutionDropdown.RefreshShownValue();
+    }
+
+
+    public void SetMasterVolume(float volume)
     {
         audioMixer.SetFloat("MasterVolume", volume);
     }
@@ -18,4 +56,21 @@ public class OptionsMenu : MonoBehaviour
     {
         audioMixer.SetFloat("SfxVolume", volume);
     }
+
+    public void SetQuality(int qualityIndex)
+    {
+        QualitySettings.SetQualityLevel(qualityIndex);
+    }
+
+    public void SetFullScreen(bool isFullScreen)
+    {
+        Screen.fullScreen = isFullScreen;
+    }
+
+    public void SetResolution(int resolutionIndex)
+    {
+        Resolution resolution = resolutions[resolutionIndex];
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
+    }
+
 }
